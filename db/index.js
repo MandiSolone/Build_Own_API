343394ecca4f4014dfad5f290030f843077f584f
[Mandi Solone] <[mandisolone@gmail.com]>
2024-09-06 13:39:28 -0400

    starting api connection

---
+++ db/index.js
@@ -0,0 +1,7 @@
+//connection object we can create queries from 
+import config from "../config";
+import mysql from "mysql"; //a default built in component
+
+const connection = mysql.createPool(config.mysql); 
+
+export default connection; 
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+// import userRouter from "./user.route"; 
+
+//Express framework creates new router objects
+//defines routes and handles request 
+const router = express.Router();
+
+router.get("/test", (req, res) => {
+  res.send("working");
+});
+
+// router.use("/users", userRouter); 
+
+export default router;
